0e51822c43a991cd087e595c4a0c59b2eb96ac89
bac1807 <65804384+bac1807@users.noreply.github.com>
2022-07-31 09:57:22 +0700

    Add files via upload

---
+++ Galaxy/config.js
@@ -0,0 +1,18 @@
+const CONFIG = {
+    introTitle: 'Babe à!',
+    introDesc: `Chúc Galaxy sinh nhật vui vẻ nhé
+    ngày càng xinh đẹp hơn nha, sinh nhật năm anh anh không bên em
+    nhưng năm sau thì sẽ khác nhaaa <3`,
+    btnIntro: 'Click here to send your love to me',
+    title: 'Anh mong là em không rời xa anh nhá 😙',
+    desc: 'Do you love Me ',
+    btnYes: 'Vẫn cứ là yêu anh <33',
+    btnNo: 'Không, Anh trai à :3',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    btnReply: 'Gửi cho anh <3',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    messDesc: 'Em dễ thương lắm đó Ngân Hà ạ',
+    btnAccept: 'Nhấn vô đây để nói yêu anh nhá :3',
+    messLink: 'https://www.facebook.com/messages/t/100006437546981' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
